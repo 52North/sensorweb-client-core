486a884b48ad49aad0766ddc06b3411c3314de4d
jansch <jansch@jansch-n52>
2015-10-01 09:37:25 +0200

    extract controller

--- src/js/Time/timeSelectorButtonsModule.js
+++ src/js/Time/timeSelectorButtonsModule.js
@@ -3,17 +3,19 @@ angular.module('n52.core.timeSelectorButtons', ['n52.core.time', 'n52.core.timeR
             return {
                 restrict: 'E',
                 templateUrl: 'templates/time/time-selector-buttons.html',
-                controller: ['$scope', 'timeService',
-                    function ($scope, timeService) {
-                        $scope.time = timeService.time;
-
-                        $scope.back = function () {
-                            timeService.stepBack();
-                        };
-
-                        $scope.forward = function () {
-                            timeService.stepForward();
-                        };
-                    }]
+                controller: 'TimeSelectorCtrl'
             };
-        });
+        })
+        .controller('TimeSelectorCtrl', ['$scope', 'timeService',
+            function ($scope, timeService) {
+                $scope.time = timeService.time;
+
+                $scope.back = function () {
+                    timeService.stepBack();
+                };
+
+                $scope.forward = function () {
+                    timeService.stepForward();
+                };
+            }]);
+                
