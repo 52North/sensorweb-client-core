5ea6cd159b2f20ccabb0b15d65f9e992a89afc7c
jansch <jansch@jansch-n52>
2015-10-15 15:27:21 +0200

    update locate controller

--- src/js/Map/locateModule.js
+++ src/js/Map/locateModule.js
@@ -1,6 +1,16 @@
 angular.module('n52.core.locate', ['n52.core.station'])
-        .controller('LocateButtonCtrl', ['$scope', 'mapService', function ($scope, mapService) {
+        .controller('SwcLocateButtonCtrl', ['$scope', 'mapService', function ($scope, mapService) {
+                $scope.isToggled = false;
+                var interval;
                 $scope.locateUser = function () {
-                    mapService.locateUser();
+                    // TODO need to be enhanced
+                    $scope.isToggled = !$scope.isToggled;
+                    if ($scope.isToggled) {
+                        interval = setInterval(function(){
+                            mapService.locateUser();
+                        }, 10000);
+                    } else {
+                        clearInterval(interval);
+                    }
                 };
             }]);
