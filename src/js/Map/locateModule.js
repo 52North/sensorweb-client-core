82242de41f140a7e0da4351ac9a2081df610240f
jansch <jansch@jansch-n52>
2015-10-15 17:30:13 +0200

    extract locate functionality to service

--- src/js/Map/locateModule.js
+++ src/js/Map/locateModule.js
@@ -1,17 +1,24 @@
 angular.module('n52.core.locate', ['n52.core.station'])
-        .controller('SwcLocateButtonCtrl', ['$scope', 'leafletData', function ($scope, leafletData) {
+        .controller('SwcLocateButtonCtrl', ['$scope', 'leafletData', 'locateService', function ($scope, leafletData, locateService) {
                 $scope.isToggled = false;
+                $scope.locateUser = function () {
+                    $scope.isToggled = !$scope.isToggled;
+                    locateService.locate($scope.isToggled);
+                };
+                
+            }])
+        .factory('locateService', ['leafletData',
+            function (leafletData) {
                 var marker;
                 leafletData.getMap().then(function (map) {
                     map.on('locationfound', function (evt, args) {
                         removeMarker(map);
                         marker = L.marker(evt.latlng, {}).addTo(map);
                     })
-                })
-                $scope.locateUser = function () {
-                    $scope.isToggled = !$scope.isToggled;
+                });
+                locate = function(search){
                     leafletData.getMap().then(function (map) {
-                        if ($scope.isToggled) {
+                        if (search) {
                             map.locate({
                                 watch: true,
                                 setView: true
@@ -21,11 +28,13 @@ angular.module('n52.core.locate', ['n52.core.station'])
                             removeMarker(map);
                         }
                     });
-                };
+                }
                 removeMarker = function (map) {
                     if (angular.isDefined(marker)) {
                         map.removeLayer(marker);
                     }
                 }
+                return {
+                    locate: locate
+                }
             }]);
-        
