1cce043ad50aa8774937288775bda5e4585dc49e
jansch <jansch@jansch-n52>
2015-10-16 08:34:35 +0200

    upgrade locate module and use a default marker or the configured one

--- src/js/Map/locateModule.js
+++ src/js/Map/locateModule.js
@@ -1,40 +1,41 @@
 angular.module('n52.core.locate', ['n52.core.station'])
-        .controller('SwcLocateButtonCtrl', ['$scope', 'leafletData', 'locateService', function ($scope, leafletData, locateService) {
+        .controller('SwcLocateButtonCtrl', ['$scope', 'locateService', function ($scope, locateService) {
                 $scope.isToggled = false;
                 $scope.locateUser = function () {
                     $scope.isToggled = !$scope.isToggled;
                     locateService.locate($scope.isToggled);
                 };
-                
             }])
-        .factory('locateService', ['leafletData',
-            function (leafletData) {
+        .factory('locateService', ['leafletData', 'settingsService',
+            function (leafletData, settingsService) {
                 var marker;
+                var locateIcon = settingsService.locateIconOptions ? L.icon(settingsService.locateIconOptions) : new L.Icon.Default();
                 leafletData.getMap().then(function (map) {
-                    map.on('locationfound', function (evt, args) {
+                    map.on('locationfound', function (evt) {
                         removeMarker(map);
-                        marker = L.marker(evt.latlng, {}).addTo(map);
-                    })
+                        marker = L.marker(evt.latlng, {icon: locateIcon}).addTo(map);
+                    });
                 });
-                locate = function(search){
+                locate = function (search) {
                     leafletData.getMap().then(function (map) {
                         if (search) {
                             map.locate({
                                 watch: true,
-                                setView: true
+                                setView: true,
+                                maxZoom: map.getZoom()
                             });
                         } else {
                             map.stopLocate();
                             removeMarker(map);
                         }
                     });
-                }
+                };
                 removeMarker = function (map) {
                     if (angular.isDefined(marker)) {
                         map.removeLayer(marker);
                     }
-                }
+                };
                 return {
                     locate: locate
-                }
+                };
             }]);
