c1e2760f1ac73e95c360734c0c138996975d7e28
jansch <j.schulte@52north.org>
2015-10-30 15:50:44 +0100

    extract controller

--- src/js/Legend/legendModule.js
+++ src/js/Legend/legendModule.js
@@ -9,53 +9,54 @@ angular.module('n52.core.legend', ['n52.core.timeseries', 'n52.core.exportTs', '
                     }]
             };
         })
-        .directive('swcLegendEntry', ['exportTsService', 'mapService', '$location', 'styleModalOpener',
-            function (exportTsService, mapService, $location, styleModalOpener) {
+        .directive('swcLegendEntry', [
+            function () {
                 return {
                     restrict: 'E',
                     templateUrl: 'templates/legend/legend-entry.html',
                     scope: {
                         timeseries: "="
                     },
-                    controller: ['$scope', 'timeseriesService', 'timeService', 'styleService', '$rootScope',
-                        function ($scope, timeseriesService, timeService, styleService, $rootScope) {
-                            $scope.infoVisible = false;
-                            $scope.toggleSelection = function (ts) {
-                                styleService.toggleSelection(ts);
-                            };
-                            $scope.removeTs = function (ts) {
-                                timeseriesService.removeTimeseries(ts.internalId);
-                            };
-                            $scope.toggleDiagram = function (ts) {
-                                styleService.toggleTimeseriesVisibility(ts);
-                            };
-                            $scope.openStyling = function (ts) {
-                                styleModalOpener(ts);
-                            };
-                            $scope.showInformation = function () {
-                                $scope.infoVisible = !$scope.infoVisible;
-                            };
-                            $scope.showInMap = function (ts) {
-                                mapService.showStation(ts);
-                                $location.url('/map');
-                            };
-                            $scope.jumpToLastTimeStamp = function (lastValue) {
-                                if (lastValue && lastValue.timestamp) {
-                                    timeService.jumpToLastTimeStamp(lastValue.timestamp, true);
-                                }
-                            };
-                            $scope.jumpToFirstTimeStamp = function (firstValue) {
-                                if (firstValue && firstValue.timestamp) {
-                                    timeService.jumpToLastTimeStamp(firstValue.timestamp, true);
-                                }
-                            };
-                            $scope.selectRefValue = function (refValue, ts) {
-                                timeseriesService.toggleReferenceValue(refValue, ts.internalId);
-                            };
-                            $scope.createExportCsv = function (id) {
-                                exportTsService.openInNewWindow(exportTsService.createCsvDownloadLink(id));
-                            };
-                        }]
+                    controller: 'SwcLegendEntryCtrl'
+                };
+            }])
+        .controller('SwcLegendEntryCtrl', ['$scope', 'timeseriesService', 'timeService', 'styleService', 'exportTsService', 'mapService', '$location', 'styleModalOpener',
+            function ($scope, timeseriesService, timeService, styleService, exportTsService, mapService, $location, styleModalOpener) {
+                $scope.infoVisible = false;
+                $scope.toggleSelection = function (ts) {
+                    styleService.toggleSelection(ts);
+                };
+                $scope.removeTs = function (ts) {
+                    timeseriesService.removeTimeseries(ts.internalId);
+                };
+                $scope.toggleDiagram = function (ts) {
+                    styleService.toggleTimeseriesVisibility(ts);
+                };
+                $scope.openStyling = function (ts) {
+                    styleModalOpener(ts);
+                };
+                $scope.showInformation = function () {
+                    $scope.infoVisible = !$scope.infoVisible;
+                };
+                $scope.showInMap = function (ts) {
+                    mapService.showStation(ts);
+                    $location.url('/map');
+                };
+                $scope.jumpToLastTimeStamp = function (lastValue) {
+                    if (lastValue && lastValue.timestamp) {
+                        timeService.jumpToLastTimeStamp(lastValue.timestamp, true);
+                    }
+                };
+                $scope.jumpToFirstTimeStamp = function (firstValue) {
+                    if (firstValue && firstValue.timestamp) {
+                        timeService.jumpToLastTimeStamp(firstValue.timestamp, true);
+                    }
+                };
+                $scope.selectRefValue = function (refValue, ts) {
+                    timeseriesService.toggleReferenceValue(refValue, ts.internalId);
+                };
+                $scope.createExportCsv = function (id) {
+                    exportTsService.openInNewWindow(exportTsService.createCsvDownloadLink(id));
                 };
             }])
         .controller('legendButtonController', ['$scope', 'statusService',
