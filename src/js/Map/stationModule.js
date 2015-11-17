444bfc809d407d7aaa19e594983cef3a8dcf0347
jansch <j.schulte@52north.org>
2015-11-17 10:10:18 +0100

    map can now render the stations for all providers simultaneously

--- src/js/Map/stationModule.js
+++ src/js/Map/stationModule.js
@@ -1,10 +1,11 @@
 angular.module('n52.core.station', ['ui.bootstrap'])
-        .controller('ModalStationCtrl', ['$scope', '$modalInstance', 'statusService', 'timeseriesService', '$location', 'stationService', 'stationId', 'phenomenonId',
-            function ($scope, $modalInstance, statusService, timeseriesService, $location, stationService, stationId, phenomenonId) {
-                stationService.determineTimeseries(stationId);
+        .controller('ModalStationCtrl', ['$scope', '$modalInstance', 'timeseriesService', '$location', 'stationService', 'selection',
+            function ($scope, $modalInstance, timeseriesService, $location, stationService, selection) {
+                debugger;
+                stationService.determineTimeseries(selection.stationId, selection.url);
                 $scope.isAllSelected = true;
                 $scope.station = stationService.station;
-                $scope.phenomenonId = phenomenonId;
+                $scope.phenomenonId = selection.phenomenonId;
 
                 $scope.toggleAll = function () {
                     angular.forEach($scope.station.entry.properties.timeseries, function (ts) {
@@ -28,7 +29,7 @@ angular.module('n52.core.station', ['ui.bootstrap'])
                 $scope.addTimeseriesSelection = function () {
                     angular.forEach($scope.station.entry.properties.timeseries, function (timeseries) {
                         if (timeseries.selected) {
-                            timeseriesService.addTimeseriesById(timeseries.id, statusService.status.apiProvider.url);
+                            timeseriesService.addTimeseriesById(timeseries.id, selection.url);
                         }
                     });
                     $location.url('/diagram');
@@ -37,34 +38,45 @@ angular.module('n52.core.station', ['ui.bootstrap'])
             }])
         .controller('StationOpenerCtrl', ['$modal', '$rootScope', 'mapService',
             function ($modal, $rootScope, mapService) {
-                $rootScope.$on('leafletDirectiveMarker.click', function (event, args) {
-                    var stationId = args.modelName;
+                clickmarker = function (event, args) {
                     $modal.open({
                         animation: true,
                         templateUrl: 'templates/map/station.html',
                         resolve: {
-                            stationId: function () {
-                                return stationId;
-                            },
-                            phenomenonId: function () {
-                                return mapService.map.selectedPhenomenonId;
+                            selection: function () {
+                                var stationsId;
+                                var url;
+                                if (args.model) {
+                                    stationsId = args.model.stationsId ? args.model.stationsId : "";
+                                    url = args.model.url ? args.model.url : "";
+                                } else if (args.leafletObject && args.leafletObject.options) {
+                                    stationsId = args.leafletObject.options.stationsId ? args.leafletObject.options.stationsId : "";
+                                    url = args.leafletObject.options.url ? args.leafletObject.options.url : "";
+                                }
+                                return {
+                                    stationId: stationsId,
+                                    phenomenonId: mapService.map.selectedPhenomenonId,
+                                    url: url
+                                };
                             }
                         },
                         controller: 'ModalStationCtrl'
                     });
-                });
+                };
+                $rootScope.$on('leafletDirectivePath.click', clickmarker);
+                $rootScope.$on('leafletDirectiveMarker.click', clickmarker);
             }])
-        .service('stationService', ['interfaceService', 'statusService',
-            function (interfaceService, statusService) {
+        .service('stationService', ['interfaceService',
+            function (interfaceService) {
                 var station = {
                     entry: {}
                 };
-                determineTimeseries = function (stationId) {
+                determineTimeseries = function (stationId, url) {
                     station.entry = {};
-                    interfaceService.getStations(stationId, statusService.status.apiProvider.url).then(function (result) {
+                    interfaceService.getStations(stationId, url).then(function (result) {
                         station.entry = result;
                         angular.forEach(result.properties.timeseries, function (timeseries, id) {
-                            interfaceService.getTimeseries(id, statusService.status.apiProvider.url).then(function (ts) {
+                            interfaceService.getTimeseries(id, url).then(function (ts) {
                                 angular.extend(timeseries, ts);
                                 timeseries.selected = true;
                             });
