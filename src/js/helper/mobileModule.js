946f6acd924e9e5fe5aaf541380d7f8c414421fc
jansch <j.schulte@52north.org>
2015-11-19 12:34:10 +0100

    remove directive clickAnywhereButHere

--- src/js/helper/mobileModule.js
+++ src/js/helper/mobileModule.js
@@ -6,24 +6,4 @@ angular.module('n52.core.mobile', [])
                         check = true;
                 })(navigator.userAgent || navigator.vendor || window.opera);
                 $scope.isMobile = check;
-            }])
-        .directive('clickAnywhereButHere', ['$document', function ($document) {
-            return {
-                restrict: 'A',
-                link: function (scope, elem, attr) {
-                    var elemClickHandler = function (e) {
-                        e.stopPropagation();
-                    };
-                    var docClickHandler = function () {
-                        scope.$apply(attr.clickAnywhereButHere);
-                    };
-                    elem.on('click', elemClickHandler);
-                    $document.on('click', docClickHandler);
-                    // teardown the event handlers when the scope is destroyed.
-                    scope.$on('$destroy', function () {
-                        elem.off('click', elemClickHandler);
-                        $document.off('click', docClickHandler);
-                    });
-                }
-            };
-        }]);
+            }]);
