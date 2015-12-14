02c89ab3bed8490ee76811b7108a6ae85ff05a83
jansch <j.schulte@52north.org>
2015-12-14 16:14:23 +0100

    update log notifier

--- src/js/helper/service/logNotifier.js
+++ src/js/helper/service/logNotifier.js
@@ -3,34 +3,38 @@ angular.module('n52.core.helper')
             function ($provide) {
                 $provide.decorator('$log', ['$delegate', 'Logging',
                     function ($delegate, Logging) {
-                        var methods =
-                                {
-                                    error: function () {
-                                        if (Logging.enabled) {
-                                            $delegate.error.apply(null, arguments);
-                                            Logging.error.apply(null, arguments);
-                                        }
-                                    },
-                                    log: function () {
-                                        if (Logging.enabled) {
-                                            $delegate.log.apply(null, arguments);
-                                            Logging.log.apply(null, arguments);
-                                        }
-                                    },
-                                    info: function () {
-                                        if (Logging.enabled) {
-                                            $delegate.info.apply(null, arguments);
-                                            Logging.info.apply(null, arguments);
-                                        }
-                                    },
-                                    warn: function () {
-                                        if (Logging.enabled) {
-                                            $delegate.warn.apply(null, arguments);
-                                            Logging.warn.apply(null, arguments);
-                                        }
-                                    }
-                                };
-                        return methods;
+                        var oldError = $delegate.error;
+                        $delegate.error = function () {
+                            if (Logging.enabled) {
+                                oldError.apply(null, arguments);
+                                Logging.error.apply(null, arguments);
+                            }
+                        };
+
+                        var oldLog = $delegate.log;
+                        $delegate.log = function () {
+                            if (Logging.enabled) {
+                                oldLog.apply(null, arguments);
+                                Logging.log.apply(null, arguments);
+                            }
+                        };
+
+                        var oldInfo = $delegate.info;
+                        $delegate.info = function () {
+                            if (Logging.enabled) {
+                                oldInfo.apply(null, arguments);
+                                Logging.info.apply(null, arguments);
+                            }
+                        };
+
+                        var oldWarn = $delegate.warn;
+                        $delegate.warn = function () {
+                            if (Logging.enabled) {
+                                oldWarn.apply(null, arguments);
+                                Logging.warn.apply(null, arguments);
+                            }
+                        };
+                        return $delegate;
                     }]);
             }])
         .service('Logging', ['$injector', 'settingsService',
@@ -39,19 +43,23 @@ angular.module('n52.core.helper')
                 var service = {
                     error: function () {
                         self.type = 'error';
-                        if (logLevel >= 1) log.apply(self, arguments);
+                        if (logLevel >= 1)
+                            log.apply(self, arguments);
                     },
                     warn: function () {
                         self.type = 'warn';
-                        if (logLevel >= 2) log.apply(self, arguments);
+                        if (logLevel >= 2)
+                            log.apply(self, arguments);
                     },
                     info: function () {
                         self.type = 'info';
-                        if (logLevel >= 3) log.apply(self, arguments);
+                        if (logLevel >= 3)
+                            log.apply(self, arguments);
                     },
                     log: function () {
                         self.type = 'log';
-                        if (logLevel >= 4) log.apply(self, arguments);
+                        if (logLevel >= 4)
+                            log.apply(self, arguments);
                     },
                     enabled: settingsService.logging ? settingsService.logging : true,
                     logLevel: 'error'
