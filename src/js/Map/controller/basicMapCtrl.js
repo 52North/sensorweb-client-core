8875520b84a4fbd7658f8f29b4bccd7f87e6b116
jansch <j.schulte@52north.org>
2016-01-07 10:43:04 +0100

    remove map search of basic map ctrl and create a new search ctrl

--- src/js/Map/controller/basicMapCtrl.js
+++ src/js/Map/controller/basicMapCtrl.js
@@ -3,25 +3,6 @@ angular.module('n52.core.map')
             function ($scope, mapService, leafletData, $translate, $rootScope) {
                 $scope.map = mapService.map;
 
-                // adds a leaflet geosearch
-                $translate(['map.search.label', 'map.search.noResult']).then(function (translations) {
-                    leafletData.getMap().then(function (map) {
-                        new L.Control.GeoSearch({
-                            url: 'http://nominatim.openstreetmap.org/search?format=json&q={s}',
-                            jsonpParam: 'json_callback',
-                            propertyName: 'display_name',
-                            searchLabel: translations['map.search.label'],
-                            notFoundMessage: translations['map.search.noResult'],
-                            propertyLoc: ['lat', 'lon'],
-                            position: 'topcenter',
-                            minLength: 2,
-                            showMarker: false,
-                            provider: new L.GeoSearch.Provider.OpenStreetMap(),
-                            zoomLevel: 13
-                        }).addTo(map);
-                    });
-                });
-                
                 $rootScope.$on('resizeMap', function () {
                     leafletData.getMap().then(function (map) {
                         map.invalidateSize(false);
