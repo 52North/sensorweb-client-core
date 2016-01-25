fe97f5822620049bae2efbaeff8146867d596dbf
jansch <j.schulte@52north.org>
2016-01-25 14:31:13 +0100

    add phenomenon attribute to phenomenon button to show selected phenomenon

--- src/js/Phenomenon/controller/phenomenaButtonCtrl.js
+++ src/js/Phenomenon/controller/phenomenaButtonCtrl.js
@@ -1,7 +1,10 @@
 angular.module('n52.core.phenomena')
-        .controller('SwcPhenomenaButtonController', ['$scope', 'statusService',
-            function ($scope, statusService) {
+        .controller('SwcPhenomenaButtonController', ['$scope', 'statusService', 'PhenomenonListFactory', 
+            function ($scope, statusService, PhenomenonListFactory) {
                 $scope.status = statusService.status;
+                
+                $scope.selectedPhenomenon = PhenomenonListFactory.phenomena;
+                
                 $scope.togglePhenomena = function () {
                     statusService.status.showPhenomena = !statusService.status.showPhenomena;
                 };
