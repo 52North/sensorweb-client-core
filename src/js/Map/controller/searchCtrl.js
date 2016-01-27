78fba82b344d27e20cb2b6b8a160bde1ccbae2bc
jansch <j.schulte@52north.org>
2016-01-27 17:29:10 +0100

    search location controller draws a marker for the search result

--- src/js/Map/controller/searchCtrl.js
+++ src/js/Map/controller/searchCtrl.js
@@ -1,8 +1,18 @@
 angular.module('n52.core.map')
-        .controller('SwcSearchCtrl', ['$scope', 'mapService', function ($scope, mapService) {
+        .controller('SwcSearchCtrl', ['$scope', 'mapService', 'nominatimService', 'leafletBoundsHelpers', 'settingsService',
+            function ($scope, mapService, nominatimService, leafletBoundsHelpers, settingsService) {
                 $scope.startSearch = function (term) {
                     if (angular.isDefined(term) && angular.isString(term)) {
-                        mapService.map.bounds.address = term;
+                        nominatimService.query(term).then(function (data) {
+                            angular.copy(leafletBoundsHelpers.createBoundsFromArray([
+                                [parseFloat(data.boundingbox[0]), parseFloat(data.boundingbox[2])],
+                                [parseFloat(data.boundingbox[1]), parseFloat(data.boundingbox[3])]]), mapService.map.bounds);
+                            mapService.map.markers.searchResult = {
+                                lat: parseFloat(data.lat),
+                                lng: parseFloat(data.lon),
+                                icon: settingsService.searchResultIconOptions ? settingsService.searchResultIconOptions : {}
+                            };
+                        });
                     }
                 };
             }]);
