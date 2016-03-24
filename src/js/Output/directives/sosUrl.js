10af03feab0b8f94825b4337ef1371895deeeb85
jansch <j.schulte@52north.org>
2016-03-24 14:26:39 +0100

    adjust template urls

--- src/js/Output/directives/sosUrl.js
+++ src/js/Output/directives/sosUrl.js
@@ -1,24 +1,26 @@
 angular.module('n52.core.sosMetadata', [])
-        .directive('swcSosUrl', [function () {
+        .directive('swcSosUrl', [
+            function () {
                 return {
                     restrict: "E",
                     replace: true,
                     scope: {
                         timeseries: "="
                     },
-                    templateUrl: "templates/output/sos-url.html",
+                    templateUrl: "templates/metadata/sos-url.html",
                     controller: 'SwcSosUrlCtrl'
                 };
             }])
         .controller('SwcSosUrlCtrl', ['$scope', 'sosUrlSrv', '$window',
             function ($scope, sosUrlSrv, $window) {
                 $scope.sos = {};
-                $scope.openCapabilities = function() {
+                $scope.openCapabilities = function () {
                     $window.open($scope.sos.url + '?request=GetCapabilities&service=SOS');
                 };
                 sosUrlSrv.getSosUrl($scope.timeseries, $scope.sos);
             }])
-        .factory('sosUrlSrv', ['interfaceService', function (interfaceService) {
+        .factory('sosUrlSrv', ['interfaceService',
+            function (interfaceService) {
                 function getSosUrl(timeseries, sos) {
                     var id = timeseries.parameters.service.id,
                             apiUrl = timeseries.apiUrl;
