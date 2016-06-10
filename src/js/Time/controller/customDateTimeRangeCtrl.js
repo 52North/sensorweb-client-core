67ac2e4ac093606481ffba039cb218e0f4316159
jansch <j.schulte@52north.org>
2016-06-10 13:53:29 +0200

    add a max time period restriction service

--- src/js/Time/controller/customDateTimeRangeCtrl.js
+++ src/js/Time/controller/customDateTimeRangeCtrl.js
@@ -1,9 +1,9 @@
 angular.module('n52.core.timeUi')
         .controller('SwcCustomDateTimeRangeCtrl', ['$scope', 'timeService',
-            function ($scope, timeService) {
-                $scope.startDate = timeService.time.start.toDate();
-                $scope.endDate = timeService.time.end.toDate();
-                $scope.confirmCustomRange = function(start, end){
-                    timeService.setFlexibleTimeExtent(moment(start), moment(end));
-                };
-            }]);
+          function ($scope, timeService) {
+            $scope.startDate = timeService.time.start.toDate();
+            $scope.endDate = timeService.time.end.toDate();
+            $scope.confirmCustomRange = function (start, end) {
+              timeService.setFlexibleTimeExtent(moment(start), moment(end));
+            };
+          }]);
---
+++ src/js/plugins/restricted-time-period.js
@@ -0,0 +1,30 @@
+// extends services to provide functionality that a user cannot select a greater time period than the settings parameter "maxTimeperiod" allows
+angular.module('n52.core.plugin.restricted-time-period', [])
+        .config(['$provide',
+          function ($provide) {
+            $provide.decorator('timeService', ['$delegate', 'Notification', '$translate', 'settingsService',
+              function ($delegate, Notification, $translate, settingsService) {
+
+                var maxTimeperiod;
+                if (settingsService.maxTimeperiod) {
+                  maxTimeperiod = moment.duration(settingsService.maxTimeperiod);
+
+                  var oldSetFelxibleTimeExtent = $delegate.setFlexibleTimeExtent;
+                  $delegate.setFlexibleTimeExtent = function (start, end) {
+                    if (!isGreaterThenMaxTimeperiod(start, end)) {
+                      oldSetFelxibleTimeExtent(start, end);
+                    } else {
+                      Notification.error($translate.instant('chart.reachedMaxTimeperiod'));
+                      var updatedStart = moment(end).subtract(maxTimeperiod);
+                      oldSetFelxibleTimeExtent(updatedStart, end);
+                    }
+                  };
+                }
+
+                isGreaterThenMaxTimeperiod = function (start, end) {
+                  return moment(start).add(maxTimeperiod).isBefore(end) ? true : false;
+                };
+
+                return $delegate;
+              }]);
+          }]);
