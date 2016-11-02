b109bd4fbff149b975cf7e98a7450226be4292cf
Travis CI <travis@travis-ci.org>
2016-11-02 11:28:48 +0100

    use i18n for table time header

--- src/js/Table/controller/tableCtrl.js
+++ src/js/Table/controller/tableCtrl.js
@@ -1,113 +1,116 @@
 angular.module('n52.core.table')
-        .controller('SwcTableCtrl', ['$scope', '$filter', 'ngTableParams', 'timeseriesService', 'timeService', '$rootScope',
-            function ($scope, $filter, ngTableParams, timeseriesService, timeService, $rootScope) {
-                // http://ngmodules.org/modules/ng-table
-                createValueArray = function () {
-                    var array = [];
-                    var map = {};
-                    var count = 0;
-                    angular.forEach(timeseriesService.getAllTimeseries(), function (ts) {
-                        var data = timeseriesService.getData(ts.internalId);
-                        if (data.values.length > 0) {
-                            var values = removeOverlappingValues(data.values);
-                            angular.forEach(values, function (pair) {
-                                var time = pair[0];
-                                var value = pair[1];
-                                if (!map[time]) {
-                                    map[time] = {
-                                        time: time
-                                    };
-                                }
-                                map[time][ts.internalId] = value;
-                            });
-                        }
-                        count++;
-                    });
-                    var i = 0;
-                    angular.forEach(map, function (entry, idx) {
-                        array[i++] = entry;
-                    });
-                    return array;
-                };
-                createColumns = function () {
-                    var columns = [];
-                    columns.push({
-                        phenomenon: 'Zeit', field: 'time', visible: true
-                    });
-                    angular.forEach(timeseriesService.getAllTimeseries(), function (ts) {
-                        var phenomenonLabel = ts.parameters.phenomenon.label;
-                        if (ts.uom) {
-                            phenomenonLabel += " (" + ts.uom + ")";
-                        }
-                        columns.push({
-                            station: ts.parameters.feature.label,
-                            phenomenon: phenomenonLabel,
-                            field: ts.internalId,
-                            color: ts.styles.color,
-                            isActive: ts.isActive
+    .controller('SwcTableCtrl', ['$scope', '$filter', 'ngTableParams', 'timeseriesService', 'timeService', '$rootScope', '$translate',
+        function($scope, $filter, ngTableParams, timeseriesService, timeService, $rootScope, $translate) {
+            // http://ngmodules.org/modules/ng-table
+            createValueArray = function() {
+                var array = [];
+                var map = {};
+                var count = 0;
+                angular.forEach(timeseriesService.getAllTimeseries(), function(ts) {
+                    var data = timeseriesService.getData(ts.internalId);
+                    if (data.values.length > 0) {
+                        var values = removeOverlappingValues(data.values);
+                        angular.forEach(values, function(pair) {
+                            var time = pair[0];
+                            var value = pair[1];
+                            if (!map[time]) {
+                                map[time] = {
+                                    time: time
+                                };
+                            }
+                            map[time][ts.internalId] = value;
                         });
-                    });
-                    return columns;
-                };
-                createTable = function () {
-                    $scope.tableParams = new ngTableParams({
-                        page: 1,
-                        count: 30,
-                        sorting: {
-                            time: 'asc'
-                        }
-                    }, {
-                        total: data.length,
-                        counts: [],
-                        getData: function ($defer, params) {
-                            var orderedData = params.sorting() ?
-                                    $filter('orderBy')(data, params.orderBy()) :
-                                    data;
-                            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-                        }
-                    });
-                };
-                removeOverlappingValues = function (values) {
-                    // remove values before start
-                    var start = timeService.getStartInMillies();
-                    var count = 0;
-                    while (values[count][0] < start)
-                        count++;
-                    values.splice(0, count);
-                    // remove values after the end
-                    var idx = values.length - 1;
-                    var end = timeService.getEndInMillies();
-                    count = 0;
-                    while (values[idx][0] > end) {
-                        count++;
-                        idx--;
                     }
-                    values.splice(++idx, count);
-                    return values;
-                };
-                $scope.loadMoreData = function () {
-                    $scope.tableParams.count($scope.tableParams.count() + 10);
-                    $scope.tableParams.reload();
-                };
-                var timeseriesChangedListener = $rootScope.$on('timeseriesChanged', function (evt, id) {
-                    data = createValueArray();
-                    $scope.columns = createColumns();
+                    count++;
                 });
-                var alltimeseriesChangedListener = $rootScope.$on('allTimeseriesChanged', function () {
-                    data = createValueArray();
-                    $scope.columns = createColumns();
+                var i = 0;
+                angular.forEach(map, function(entry, idx) {
+                    array[i++] = entry;
                 });
-                var timeseriesDataChangedListener = $rootScope.$on('timeseriesDataChanged', function (evt, id) {
-                    data = createValueArray();
-                    $scope.columns = createColumns();
-                    $scope.tableParams.reload();
+                return array;
+            };
+            createColumns = function() {
+                var columns = [];
+                columns.push({
+                    phenomenon: $translate.instant('table.time'),
+                    field: 'time',
+                    visible: true
                 });
+                angular.forEach(timeseriesService.getAllTimeseries(), function(ts) {
+                    var phenomenonLabel = ts.parameters.phenomenon.label;
+                    if (ts.uom) {
+                        phenomenonLabel += " (" + ts.uom + ")";
+                    }
+                    columns.push({
+                        station: ts.parameters.feature.label,
+                        phenomenon: phenomenonLabel,
+                        field: ts.internalId,
+                        color: ts.styles.color,
+                        isActive: ts.isActive
+                    });
+                });
+                return columns;
+            };
+            createTable = function() {
+                $scope.tableParams = new ngTableParams({
+                    page: 1,
+                    count: 30,
+                    sorting: {
+                        time: 'asc'
+                    }
+                }, {
+                    total: data.length,
+                    counts: [],
+                    getData: function($defer, params) {
+                        var orderedData = params.sorting() ?
+                            $filter('orderBy')(data, params.orderBy()) :
+                            data;
+                        $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                    }
+                });
+            };
+            removeOverlappingValues = function(values) {
+                // remove values before start
+                var start = timeService.getStartInMillies();
+                var count = 0;
+                while (values[count][0] < start)
+                    count++;
+                values.splice(0, count);
+                // remove values after the end
+                var idx = values.length - 1;
+                var end = timeService.getEndInMillies();
+                count = 0;
+                while (values[idx][0] > end) {
+                    count++;
+                    idx--;
+                }
+                values.splice(++idx, count);
+                return values;
+            };
+            $scope.loadMoreData = function() {
+                $scope.tableParams.count($scope.tableParams.count() + 10);
+                $scope.tableParams.reload();
+            };
+            var timeseriesChangedListener = $rootScope.$on('timeseriesChanged', function(evt, id) {
+                data = createValueArray();
                 $scope.columns = createColumns();
-                $scope.$on('$destroy', function () {
-                    timeseriesChangedListener();
-                    timeseriesDataChangedListener();
-                    alltimeseriesChangedListener();
-                });
-                var data = createValueArray();
-                createTable();
-            }]);
+            });
+            var alltimeseriesChangedListener = $rootScope.$on('allTimeseriesChanged', function() {
+                data = createValueArray();
+                $scope.columns = createColumns();
+            });
+            var timeseriesDataChangedListener = $rootScope.$on('timeseriesDataChanged', function(evt, id) {
+                data = createValueArray();
+                $scope.columns = createColumns();
+                $scope.tableParams.reload();
+            });
+            $scope.columns = createColumns();
+            $scope.$on('$destroy', function() {
+                timeseriesChangedListener();
+                timeseriesDataChangedListener();
+                alltimeseriesChangedListener();
+            });
+            var data = createValueArray();
+            createTable();
+        }
+    ]);
