3daa4ae03d0bebe6bd1b32f5b923be4814e352de
Travis CI <travis@travis-ci.org>
2016-11-02 13:29:33 +0100

    move additionalProvider to core

---
+++ src/js/Provider/controller/additionalProvider.js
@@ -0,0 +1,33 @@
+angular.module('n52.core.provider')
+    .component('swcAddProvider', {
+        templateUrl: 'templates/provider/add-provider.html',
+        controller: ['providerService', '$timeout',
+            function(providerService, $timeout) {
+                var ctrl = this;
+                ctrl.load = {};
+                //ctrl.providerUrl = 'http://sensorweb.demo.52north.org/sensorwebclient-webapp-stable/api/v1/';
+                ctrl.addProvider = function() {
+                    providerService.addProvider(ctrl.providerUrl)
+                        .then(res => {
+                            ctrl.addSuccessful();
+                        }, error => {
+                            ctrl.unsuccessful();
+                        });
+                };
+
+                ctrl.addSuccessful = function() {
+                    ctrl.load.successful = true;
+                    $timeout(() => {
+                        ctrl.load.successful = false;
+                    }, 2000);
+                };
+
+                ctrl.unsuccessful = function() {
+                    ctrl.load.failed = true;
+                    $timeout(() => {
+                        ctrl.load.failed = false;
+                    }, 2000);
+                };
+            }
+        ]
+    });
