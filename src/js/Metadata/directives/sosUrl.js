b4a8a3db4b1d7bd512a80e4ac7f77a39e13ecaf8
Jan Schulte <j.schulte@52north.org>
2016-11-18 13:50:52 +0100

    move service

--- src/js/Metadata/directives/sosUrl.js
+++ src/js/Metadata/directives/sosUrl.js
@@ -14,17 +14,4 @@ angular.module('n52.core.sosMetadata', [])
             };
             sosUrlSrv.getSosUrl(this.timeseries, this.sos);
         }
-    ])
-    .service('sosUrlSrv', ['interfaceService',
-        function(interfaceService) {
-            this.getSosUrl = function(timeseries, sos) {
-                var id = timeseries.parameters.service.id,
-                    apiUrl = timeseries.apiUrl;
-                interfaceService.getServices(apiUrl, id).then(function(service) {
-                    if (service.type === 'SOS') {
-                        sos.url = service.serviceUrl;
-                    }
-                });
-            };
-        }
     ]);
