bd5f4e4ce500bcdf0e635ef26ebc8f113b63e4d1
Jan Schulte <j.schulte@52north.org>
2017-01-05 16:26:40 +0100

    fix error in sosUrlSrv

--- src/js/Metadata/metadata.js
+++ src/js/Metadata/metadata.js
@@ -2,13 +2,15 @@ angular.module('n52.core.metadata', [])
     .service('sosUrlSrv', ['interfaceService',
         function(interfaceService) {
             this.getSosUrl = function(timeseries, sos) {
-                var id = timeseries.parameters.service.id,
-                    apiUrl = timeseries.apiUrl;
-                interfaceService.getServices(apiUrl, id).then(function(service) {
-                    if (service.type === 'SOS') {
-                        sos.url = service.serviceUrl;
-                    }
-                });
+                if (timeseries) {
+                    var id = timeseries.parameters.service.id,
+                        apiUrl = timeseries.apiUrl;
+                    interfaceService.getServices(apiUrl, id).then(function(service) {
+                        if (service.type === 'SOS') {
+                            sos.url = service.serviceUrl;
+                        }
+                    });
+                }
             };
         }
     ]);
