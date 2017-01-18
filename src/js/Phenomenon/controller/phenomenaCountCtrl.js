9cce0ca24980807e9c7d2cc6467ad060f814cae3
Jan Schulte <j.schulte@52north.org>
2017-01-18 08:10:56 +0100

    fix SwcPhenomenaCountCtrl

--- src/js/Phenomenon/controller/phenomenaCountCtrl.js
+++ src/js/Phenomenon/controller/phenomenaCountCtrl.js
@@ -1,10 +1,15 @@
 angular.module('n52.core.phenomena')
-        .controller('SwcPhenomenaCountCtrl', ['$scope', 'interfaceService', 'statusService',
-            function ($scope, interfaceService, statusService) {
-                $scope.count = null;
-                $scope.getCount = function (phenomenon) {
-                    interfaceService.getStations(null, statusService.status.apiProvider.url, {phenomenon: phenomenon.id}).then(function (data) {
-                        $scope.count = data.length;
+    .controller('SwcPhenomenaCountCtrl', ['$scope', 'interfaceService', 'statusService',
+        function($scope, interfaceService, statusService) {
+            $scope.count = 0;
+            $scope.getCount = function(phenomenon) {
+                phenomenon.provider.forEach(provider => {
+                    interfaceService.getStations(null, provider.url, {
+                        phenomenon: provider.phenomenonID
+                    }).then(function(data) {
+                        $scope.count = $scope.count + data.length;
                     });
-                };
-            }]);
+                });
+            };
+        }
+    ]);
