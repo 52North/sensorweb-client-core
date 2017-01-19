78aafaa684239ff3436927443b45a71f4dd4882b
Jan Schulte <j.schulte@52north.org>
2017-01-19 09:40:39 +0100

    remove unused files

--- src/js/ListSelection/controller/listSelectionButtonCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('n52.core.listSelection')
-        .controller('SwcListSelectionButtonCtrl', ['$scope', 'modalOpener',
-            function ($scope, modalOpener) {
-                $scope.openListSelection = function () {
-                    modalOpener.open({
-                        templateUrl: 'n52.core.listSelection.modal-list-selection'
-                    });
-                };
-            }]);
