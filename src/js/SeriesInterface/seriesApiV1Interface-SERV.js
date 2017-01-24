2a49821b305cad095d2d648297e784f3b81fc275
Jan Schulte <j.schulte@52north.org>
2017-01-24 16:36:21 +0100

    fix revamp methode in interfaceApiV1

--- src/js/SeriesInterface/seriesApiV1Interface-SERV.js
+++ src/js/SeriesInterface/seriesApiV1Interface-SERV.js
@@ -1,16 +1,16 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('n52.core.interface').service('seriesApiV1Interface', [
         '$http',
         'utils',
         'interfaceUtils',
-        function ($http, utils, interfaceUtils) {
+        function($http, utils, interfaceUtils) {
 
-            this.getServices = function (id, apiUrl, params) {
+            this.getServices = function(id, apiUrl, params) {
 
                 var requestParams,
-                        requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'services/', id);
+                    requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'services/', id);
 
                 params = this.extendParams(params, {
                     expanded: true
@@ -21,70 +21,70 @@
                 return interfaceUtils.requestSeriesApi(requestUrl, requestParams);
             };
 
-            this.getStations = function (id, apiUrl, params) {
+            this.getStations = function(id, apiUrl, params) {
 
                 var requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'stations/', id),
-                        requestParams = interfaceUtils.createRequestConfigs(params);
+                    requestParams = interfaceUtils.createRequestConfigs(params);
 
                 return interfaceUtils.requestSeriesApi(requestUrl, requestParams);
             };
 
-            this.getPhenomena = function (id, apiUrl, params) {
+            this.getPhenomena = function(id, apiUrl, params) {
 
                 var requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'phenomena/', id),
-                        requestParams = interfaceUtils.createRequestConfigs(params);
+                    requestParams = interfaceUtils.createRequestConfigs(params);
 
                 return interfaceUtils.requestSeriesApi(requestUrl, requestParams);
             };
 
 
-            this.getCategories = function (id, apiUrl, params) {
+            this.getCategories = function(id, apiUrl, params) {
 
                 var requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'categories/', id),
-                        requestParams = interfaceUtils.createRequestConfigs(params);
+                    requestParams = interfaceUtils.createRequestConfigs(params);
 
                 return interfaceUtils.requestSeriesApi(requestUrl, requestParams);
             };
 
-            this.getFeatures = function (id, apiUrl, params) {
+            this.getFeatures = function(id, apiUrl, params) {
 
                 var requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'features/', id),
-                        requestParams = interfaceUtils.createRequestConfigs(params);
+                    requestParams = interfaceUtils.createRequestConfigs(params);
 
                 return interfaceUtils.requestSeriesApi(requestUrl, requestParams);
 
             };
 
-            this.getProcedures = function (id, apiUrl, params) {
+            this.getProcedures = function(id, apiUrl, params) {
 
                 var requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'procedures/', id),
-                        requestParams = interfaceUtils.createRequestConfigs(params);
+                    requestParams = interfaceUtils.createRequestConfigs(params);
 
                 return interfaceUtils.requestSeriesApi(requestUrl, requestParams);
             };
 
-            this.getOfferings = function (id, apiUrl, params) {
+            this.getOfferings = function(id, apiUrl, params) {
 
                 var requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'offerings/', id),
-                        requestParams = interfaceUtils.createRequestConfigs(params);
+                    requestParams = interfaceUtils.createRequestConfigs(params);
 
                 return interfaceUtils.requestSeriesApi(requestUrl, requestParams);
             };
 
-            this.search = function (apiUrl, arrayParams) {
+            this.search = function(apiUrl, arrayParams) {
 
                 var requestUrl = apiUrl + 'search',
-                        requestParams = interfaceUtils.createRequestConfigs({
-                            q: arrayParams.join(',')
-                        });
+                    requestParams = interfaceUtils.createRequestConfigs({
+                        q: arrayParams.join(',')
+                    });
 
                 return interfaceUtils.requestSeriesApi(requestUrl, requestParams);
             };
 
-            this.getTimeseries = function (id, apiUrl, params) {
+            this.getTimeseries = function(id, apiUrl, params) {
 
                 var requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'timeseries/', id),
-                        requestParams = params || {};
+                    requestParams = params || {};
 
                 requestParams.expanded = true;
                 requestParams.force_latest_values = true;
@@ -92,7 +92,7 @@
                 requestParams.rendering_hints = true;
 
                 return $http.get(requestUrl, interfaceUtils.createRequestConfigs(requestParams))
-                        .then(response => {
+                    .then(response => {
                             if (angular.isArray(response.data)) {
                                 angular.forEach(response.data, ts => {
                                     ts.apiUrl = apiUrl;
@@ -103,19 +103,19 @@
                                 return response.data;
                             }
                         },
-                                interfaceUtils.errorCallback
-                                );
+                        interfaceUtils.errorCallback
+                    );
             };
 
-            this.getTsData = function (id, apiUrl, timespan, extendedData, generalizeData, expanded) {
+            this.getTsData = function(id, apiUrl, timespan, extendedData, generalizeData, expanded) {
 
                 var requestUrl = interfaceUtils.createRequestUrl(apiUrl, 'timeseries/', id) + "/getData",
-                        requestParams, params = {
-                            timespan: utils.createRequestTimespan(timespan.start, timespan.end),
-                            generalize: generalizeData || false,
-                            expanded: true,
-                            format: 'flot'
-                        };
+                    requestParams, params = {
+                        timespan: utils.createRequestTimespan(timespan.start, timespan.end),
+                        generalize: generalizeData || false,
+                        expanded: true,
+                        format: 'flot'
+                    };
 
                 if (extendedData) {
                     angular.extend(params, extendedData);
@@ -128,43 +128,40 @@
                 requestParams = interfaceUtils.createRequestConfigs(params);
 
                 return $http.get(requestUrl, requestParams)
-                        .then(
-                                function (response) {
-                                    console.log(response);
-                                    revampTimeseriesData(response.data, id);
-                                    return response.data;
-                                },
-                                interfaceUtils.errorCallback
-                                );
+                    .then(
+
+                        function(response) {
+                            console.log(response);
+                            revampTimeseriesData(response.data, id);
+                            return response.data;
+                        },
+                        interfaceUtils.errorCallback
+                    );
             };
 
-            function revampTimeseriesData(data, id, expanded) {
-
+            function revampTimeseriesData(data, id) {
                 var temp = [];
-
-                if (expanded && data[id].values.length > 0 && data[id].values[0].timestamp) {
-
-                    angular.forEach(data[id].values,
-                            function (entry) {
-                                temp.push([entry.timestamp, entry.value]);
-                            }
-                    );
+                if (data[id] &&
+                    data[id].values &&
+                    data[id].values.length > 0 &&
+                    data[id].values[0].timestamp) {
+                    angular.forEach(data[id].values, entry => {
+                        temp.push([entry.timestamp, entry.value]);
+                    });
                     data[id].values = temp;
                 }
-
-                if (!expanded && data.values.length > 0 && data.values[0].timestamp) {
-
-                    angular.forEach(data[id].values,
-                            function (entry) {
-                                temp.push([entry.timestamp, entry.value]);
-                            }
-                    );
+                if (data &&
+                    data.values &&
+                    data.values.length > 0 &&
+                    data.values[0].timestamp) {
+                    angular.forEach(data.values, entry => {
+                        temp.push([entry.timestamp, entry.value]);
+                    });
                     data.values = temp;
                 }
             }
 
-
-            this.extendParams = function (params, extendParams) {
+            this.extendParams = function(params, extendParams) {
                 if (!params) {
                     return extendParams;
                 } else {
