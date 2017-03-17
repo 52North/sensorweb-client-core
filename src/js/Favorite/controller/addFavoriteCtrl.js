6ffb334e0f0266f4452f1ba7a94c01b98affe3b1
Jan Schulte <j.schulte@52north.org>
2017-03-17 09:35:02 +0100

    add label-mapper and use it in flotChartServ for uom label and in favoriteCtrl for favorite selection notification

--- src/js/Favorite/controller/addFavoriteCtrl.js
+++ src/js/Favorite/controller/addFavoriteCtrl.js
@@ -1,17 +1,21 @@
 angular.module('n52.core.favoriteUi')
-        .controller('SwcAddFavoriteCtrl', ['$scope', '$translate', 'favoriteService', 'Notification', function ($scope, $translate, favoriteService, Notification) {
-                $scope.isFavorite = favoriteService.hasFavorite($scope.timeseries.internalId);
-                $scope.toggleFavorite = function () {
-                    var label;
-                    if ($scope.isFavorite) {
-                        favoriteService.removeFavorite($scope.timeseries.internalId);
-                        label = $scope.timeseries.label;
+    .controller('SwcAddFavoriteCtrl', ['$scope', '$translate', 'favoriteService', 'Notification', 'labelMapperSrvc',
+        function($scope, $translate, favoriteService, Notification, labelMapperSrvc) {
+            $scope.isFavorite = favoriteService.hasFavorite($scope.timeseries.internalId);
+            $scope.toggleFavorite = function() {
+                var label;
+                if ($scope.isFavorite) {
+                    favoriteService.removeFavorite($scope.timeseries.internalId);
+                    labelMapperSrvc.getMappedLabel($scope.timeseries.label).then((label) => {
                         Notification.primary($translate.instant('favorite.single.remove').replace('{0}', label));
-                    } else {
-                        favoriteService.addFavorite($scope.timeseries);
-                        label = $scope.timeseries.label;
+                    });
+                } else {
+                    favoriteService.addFavorite($scope.timeseries);
+                    labelMapperSrvc.getMappedLabel($scope.timeseries.label).then((label) => {
                         Notification.primary($translate.instant('favorite.single.add').replace('{0}', label));
-                    }
-                    $scope.isFavorite = favoriteService.hasFavorite($scope.timeseries.internalId);
-                };
-            }]);
+                    });
+                }
+                $scope.isFavorite = favoriteService.hasFavorite($scope.timeseries.internalId);
+            };
+        }
+    ]);
---
+++ src/js/Legend/components/label-mapper.js
@@ -0,0 +1,59 @@
+angular.module('n52.core.legend')
+    .component('swcLabelMapper', {
+        bindings: {
+            label: '<'
+        },
+        templateUrl: 'n52.core.legend.label-mapper',
+        controller: ['$http', '$q', 'labelMapperSrvc',
+            function($http, $q, labelMapperSrvc) {
+                this.$onInit = function() {
+                    labelMapperSrvc.getMappedLabel(this.label).then((label) => {
+                        this.determinedLabel = label;
+                    });
+                };
+            }
+        ]
+    })
+    .service('labelMapperSrvc', ['$q', '$http', 'settingsService',
+        function($q, $http, settingsService) {
+
+            var findUrls = function(text) {
+                var source = (text || '').toString();
+                var urlArray = [];
+                var url;
+                var matchArray;
+                var regexToken = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?&\/\/=]+)/g;
+                while ((matchArray = regexToken.exec(source)) !== null) {
+                    var token = matchArray[0];
+                    urlArray.push(token);
+                }
+                return urlArray;
+            };
+
+            this.getMappedLabel = function(label) {
+                return $q(function(resolve, reject) {
+                    if (!settingsService.solveLabels) {
+                        resolve(label);
+                    }
+                    var urls = findUrls(label);
+                    if (urls.length > 0) {
+                        var request = [];
+                        urls.forEach((url) => {
+                            labelUrl = settingsService.proxyUrl ? settingsService.proxyUrl + url : url;
+                            request.push($http.get(labelUrl, {
+                                cache: true
+                            }).then((response) => {
+                                var xml = jQuery.parseXML(response.data);
+                                label = label.replace(url, $(xml).find('prefLabel').text());
+                            }, (error) => {}));
+                        });
+                        $q.all(request).then(() => {
+                            resolve(label);
+                        });
+                    } else {
+                        resolve(label);
+                    }
+                });
+            };
+        }
+    ]);
