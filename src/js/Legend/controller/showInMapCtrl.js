c90a6869f0546a63cb6d79dca56b7b95471f5528
Jan Schulte <j.schulte@52north.org>
2017-03-21 10:22:19 +0100

    support locate platform

--- src/js/Legend/controller/showInMapCtrl.js
+++ src/js/Legend/controller/showInMapCtrl.js
@@ -1,8 +1,9 @@
 angular.module('n52.core.legend')
-        .controller('SwcShowInMapCtrl', ['$scope', 'locateStationService', '$location',
-            function ($scope, locateStationService, $location) {
-                $scope.showInMap = function (ts) {
-                    locateStationService.showStation('mapService', ts);
-                    $location.url('/map');
-                };
-            }]);
+    .controller('SwcShowInMapCtrl', ['$scope', 'locateStationService', '$location',
+        function($scope, locateStationService, $location) {
+            $scope.showInMap = function(ts) {
+                locateStationService.showStation('mapService', ts);
+                $location.url('/map');
+            };
+        }
+    ]);
--- src/js/Map/directives/locateStation.js
+++ src/js/Map/directives/locateStation.js
@@ -1,45 +1,80 @@
 angular.module('n52.core.map')
-        .service('locateStationService', ['$injector', '$rootScope', 'settingsService',
-            function ($injector, $rootScope, settingsService) {
-                this.showStation = function (mapService, timeseries) {
-                    var service = $injector.get(mapService);
-                    var map = service.map;
-                    if (timeseries && timeseries.station) {
-                        var selectedStation = {
-                            lat: timeseries.station.geometry.coordinates[1],
-                            lng: timeseries.station.geometry.coordinates[0],
-                            focus: true,
-                            clickable: false,
-                            icon: settingsService.stationIconOptions,
-                            message: '<swc-locate-station timeseriesid="' + timeseries.internalId + '"></swc-locate-station>'
-                        };
-                        map.markers.selectedStation = selectedStation;
-                        map.center = {
-                            lat: timeseries.station.geometry.coordinates[1],
-                            lng: timeseries.station.geometry.coordinates[0],
-                            zoom: 12
-                        };
-                        var popupClosed = $rootScope.$on('leafletDirectiveMap.' + map.id + '.popupclose', function () {
-                            popupClosed();
-                            mapClosed();
-                            delete map.markers.selectedStation;
-                        });
-                        var mapClosed = $rootScope.$on('leafletDirectiveMap.' + map.id + '.unload', function () {
-                            popupClosed();
-                            mapClosed();
-                            delete map.markers.selectedStation;
-                        });
-                    }
+    .service('locateStationService', ['$injector', '$rootScope', 'settingsService', 'seriesApiInterface',
+        function($injector, $rootScope, settingsService, seriesApiInterface) {
+
+            var createCloseEvents = function(map) {
+                var popupClosed = $rootScope.$on('leafletDirectiveMap.' + map.id + '.popupclose', function() {
+                    popupClosed();
+                    mapClosed();
+                    delete map.markers.selectedStation;
+                });
+                var mapClosed = $rootScope.$on('leafletDirectiveMap.' + map.id + '.unload', function() {
+                    popupClosed();
+                    mapClosed();
+                    delete map.markers.selectedStation;
+                });
+            };
+
+            var centerMap = function(map, coordinates) {
+                map.center = {
+                    lat: coordinates[1],
+                    lng: coordinates[0],
+                    zoom: 12
                 };
-            }])
-        .directive('swcLocateStation', ['timeseriesService',
-            function (timeseriesService) {
-                return {
-                    restrict: 'E',
-                    templateUrl: 'n52.core.map.locate-station',
-                    scope: {},
-                    link: function (scope, element, attrs) {
-                        scope.timeseries = timeseriesService.getTimeseries(attrs.timeseriesid);
-                    }
-                };
-            }]);
+            };
+
+            var setSelection = function(map, selection) {
+                map.markers.selectedStation = selection;
+            };
+
+            this.showStation = function(mapService, timeseries) {
+                var service = $injector.get(mapService);
+                var map = service.map;
+                if (timeseries && timeseries.station) {
+                    var selectedStation = {
+                        lat: timeseries.station.geometry.coordinates[1],
+                        lng: timeseries.station.geometry.coordinates[0],
+                        focus: true,
+                        clickable: false,
+                        icon: settingsService.stationIconOptions,
+                        message: '<swc-locate-station timeseriesid="' + timeseries.internalId + '"></swc-locate-station>'
+                    };
+                    setSelection(map, selectedStation);
+                    centerMap(map, timeseries.station.geometry.coordinates);
+                    createCloseEvents(map);
+                }
+            };
+            this.showPlatform = function(mapService, dataset) {
+                var service = $injector.get(mapService);
+                var map = service.map;
+                if (dataset && dataset.seriesParameters.platform) {
+                    seriesApiInterface.getPlatforms(dataset.seriesParameters.platform.id, dataset.apiUrl)
+                        .then((platform) => {
+                            var selectedPlatform = {
+                                lat: platform.geometry.coordinates[1],
+                                lng: platform.geometry.coordinates[0],
+                                focus: true,
+                                clickable: false,
+                                icon: settingsService.stationIconOptions,
+                                message: '<swc-locate-station timeseriesid="' + dataset.internalId + '"></swc-locate-station>'
+                            };
+                            setSelection(map, selectedPlatform);
+                            centerMap(map, platform.geometry.coordinates);
+                            createCloseEvents(map);
+                        });
+                }
+            };
+        }
+    ])
+    .directive('swcLocateStation', ['timeseriesService',
+        function(timeseriesService) {
+            return {
+                restrict: 'E',
+                templateUrl: 'n52.core.map.locate-station',
+                scope: {},
+                link: function(scope, element, attrs) {
+                    scope.timeseries = timeseriesService.getTimeseries(attrs.timeseriesid);
+                }
+            };
+        }
+    ]);
