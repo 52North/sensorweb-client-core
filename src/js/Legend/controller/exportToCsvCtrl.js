4f24da7b8a99afbe1b5d7614545b2161f63ce2a7
Jan Schulte <j.schulte@52north.org>
2017-04-04 09:21:21 +0200

    update csv export to support datasets

--- src/js/Legend/controller/exportToCsvCtrl.js
+++ src/js/Legend/controller/exportToCsvCtrl.js
@@ -1,7 +1,8 @@
 angular.module('n52.core.exportTs')
-        .controller('SwcExportToCsvCtrl', ['$scope', 'exportTsService',
-            function ($scope, exportTsService) {
-                $scope.createExportCsv = function (series) {
-                    exportTsService.openInNewWindow(exportTsService.createCsvDownloadLink(series));
-                };
-            }]);
+    .controller('SwcExportToCsvCtrl', ['$scope', 'exportTsService',
+        function($scope, exportTsService) {
+            $scope.createExportCsv = function(series) {
+                exportTsService.openInNewWindow(exportTsService.createCsvDownloadLink(series));
+            };
+        }
+    ]);
--- src/js/Legend/exportTsModule.js
+++ src/js/Legend/exportTsModule.js
@@ -1,24 +1,30 @@
 angular.module('n52.core.exportTs', [])
-        .factory('exportTsService', ['statusService', '$window', '$translate', 'utils',
-            function (statusService, $window, $translate, utils) {
+    .factory('exportTsService', ['statusService', '$window', '$translate', 'utils',
+        function(statusService, $window, $translate, utils) {
 
-                function createCsvDownloadLink(ts) {
-                    var apiUrl = ts.apiUrl, tsId = ts.id;
-                    var timespan = statusService.getTime();
-                    var kvp = '?generalize=' + false;
-                    kvp = kvp + '&timespan=' + encodeURIComponent(utils.createRequestTimespan(timespan.start, timespan.end));
-                    kvp = kvp + '&locale=' + $translate.preferredLanguage();
-                    kvp = kvp + '&zip=true';
-                    kvp = kvp + '&bom=true';
+            function createCsvDownloadLink(ts) {
+                var apiUrl = ts.apiUrl,
+                    tsId = ts.id;
+                var timespan = statusService.getTime();
+                var kvp = '?generalize=' + false;
+                kvp = kvp + '&timespan=' + encodeURIComponent(utils.createRequestTimespan(timespan.start, timespan.end));
+                kvp = kvp + '&locale=' + $translate.preferredLanguage();
+                kvp = kvp + '&zip=true';
+                kvp = kvp + '&bom=true';
+                if (ts.datasetType) {
+                    return apiUrl + 'datasets/' + tsId + '/data.zip' + kvp;
+                } else {
                     return apiUrl + 'timeseries/' + tsId + '/getData.zip' + kvp;
                 }
+            }
 
-                function openInNewWindow(link) {
-                    $window.open(link);
-                }
+            function openInNewWindow(link) {
+                $window.open(link);
+            }
 
-                return {
-                    openInNewWindow: openInNewWindow,
-                    createCsvDownloadLink: createCsvDownloadLink
-                };
-            }]);
+            return {
+                openInNewWindow: openInNewWindow,
+                createCsvDownloadLink: createCsvDownloadLink
+            };
+        }
+    ]);
