5777561c027b36e33647e0ddc43e138a5be1a417
Jan Schulte <j.schulte@52north.org>
2017-04-12 13:31:20 +0200

    improve selectableStationEntry

--- src/js/Map/components/selectableStationEntry.js
+++ src/js/Map/components/selectableStationEntry.js
@@ -9,6 +9,16 @@ angular.module('n52.core.map')
         controller: ['seriesApiInterface', 'utils',
             function(seriesApiInterface, utils) {
                 this.$onInit = function() {
+                    this.fetchTimeseries();
+                };
+
+                this.$onChanges = function(changeObj) {
+                    if (changeObj.timeseriesId || changeObj.timeseries) {
+                        this.fetchTimeseries();
+                    }
+                };
+
+                this.fetchTimeseries = function() {
                     if (this.timeseriesId && this.timeseries) {
                         seriesApiInterface.getTimeseries(this.timeseriesId, this.serviceUrl).then((ts) => {
                             ts.internalId = utils.createInternalId(ts);
