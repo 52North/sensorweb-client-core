a49e2b497529b3831206504bdd6642f39418313d
Jan Schulte <j.schulte@52north.org>
2017-05-22 11:20:59 +0200

    fix wrong using of error callback method

--- src/js/SeriesInterface/interfaceUtils-SERV.js
+++ src/js/SeriesInterface/interfaceUtils-SERV.js
@@ -1,81 +1,79 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('n52.core.interface')
-            .service('interfaceUtils', ['settingsService', '$http', "$log",
-                function (settingsService, $http, $log) {
+        .service('interfaceUtils', ['settingsService', '$http', '$log', '$q',
+            function(settingsService, $http, $log, $q) {
 
-                    var that = this;
+                this.createRequestUrl = function(apiUrl, endpoint, id) {
 
-                    this.createRequestUrl = function (apiUrl, endpoint, id) {
+                    // TODO Check whether apiUrl ends with slash
+                    var requestUrl = apiUrl + endpoint;
 
-                        // TODO Check whether apiUrl ends with slash
-                        var requestUrl = apiUrl + endpoint;
+                    if (id) {
+                        requestUrl += id;
+                    }
+                    return requestUrl;
+                };
 
-                        if (id) {
-                            requestUrl += id;
-                        }
-                        return requestUrl;
+                this.createRequestConfigs = function(params) {
+                    if (angular.isUndefined(params) || params === null) {
+                        params = settingsService.additionalParameters;
+                    } else {
+                        angular.extend(params, settingsService.additionalParameters);
+                    }
+                    var cache = true;
+                    if (typeof(params.cache) === "boolean") {
+                        cache = params.cache;
+                        delete params.cache;
+                    }
+                    return {
+                        params: params,
+                        cache: cache
                     };
+                };
 
-                    this.createRequestConfigs = function (params) {
-                        if (angular.isUndefined(params) || params === null) {
-                            params = settingsService.additionalParameters;
-                        } else {
-                            angular.extend(params, settingsService.additionalParameters);
-                        }
-                        var cache = true;
-                        if (typeof(params.cache) === "boolean") {
-                          cache = params.cache;
-                          delete params.cache;
-                        }
-                        return {
-                            params: params,
-                            cache: cache
-                        };
-                    };
+                this.requestSeriesApi = function(requestUrl, params) {
+                    return $q((resolve, reject) => {
+                        $http.get(requestUrl, params)
+                            .then(
+                                response => resolve(response.data),
+                                error => this.errorCallback(error, reject)
+                            );
+                    });
+                };
 
-                    this.requestSeriesApi = function (requestUrl, params) {
-                        return $http.get(requestUrl, params)
-                                .then(
-                                        function (response) {
-                                            return response.data;
-                                        },
-                                        that.errorCallback
-                                        );
-                    };
+                this.errorCallback = function(error, reject) {
+                    if (error.data && error.data.userMessage) {
+                        $log.error(error.data.userMessage);
+                    }
+                    if (!angular.isUndefined(reject)) {
+                        reject(error);
+                    }
+                };
 
-                    this.errorCallback = function (error, reject) {
-                        if (error.data && error.data.userMessage) {
-                            $log.error(error.data.userMessage);
-                        }
-                        if (!angular.isUndefined(reject)) {
-                            reject(error);
-                        }
-                    };
+                this.revampTimeseriesData = function(data, id) {
+                    var temp = [];
+                    if (data[id] &&
+                        data[id].values &&
+                        data[id].values.length > 0 &&
+                        data[id].values[0].timestamp) {
+                        angular.forEach(data[id].values, entry => {
+                            temp.push([entry.timestamp, entry.value]);
+                        });
+                        data[id].values = temp;
+                    }
+                    if (data &&
+                        data.values &&
+                        data.values.length > 0 &&
+                        data.values[0].timestamp) {
+                        angular.forEach(data.values, entry => {
+                            temp.push([entry.timestamp, entry.value]);
+                        });
+                        data.values = temp;
+                    }
+                };
 
-                    this.revampTimeseriesData = function(data, id) {
-                        var temp = [];
-                        if (data[id] &&
-                            data[id].values &&
-                            data[id].values.length > 0 &&
-                            data[id].values[0].timestamp) {
-                            angular.forEach(data[id].values, entry => {
-                                temp.push([entry.timestamp, entry.value]);
-                            });
-                            data[id].values = temp;
-                        }
-                        if (data &&
-                            data.values &&
-                            data.values.length > 0 &&
-                            data.values[0].timestamp) {
-                            angular.forEach(data.values, entry => {
-                                temp.push([entry.timestamp, entry.value]);
-                            });
-                            data.values = temp;
-                        }
-                    };
-
-                }
-            ]);
+            }
+        ]);
 }());
