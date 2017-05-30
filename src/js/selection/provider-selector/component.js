a5dc04d72ed8ae0dbca9e859d6e4af9bda6efb30
Jan Schulte <j.schulte@52north.org>
2017-05-30 07:29:30 +0200

    add provider selector component

---
+++ src/js/selection/provider-selector/component.js
@@ -0,0 +1,43 @@
+require('../selection');
+require('./service');
+angular.module('n52.core.selection')
+    .component('providerSelector', {
+        bindings: {
+            providerList: '<',
+            providerBlacklist: '<',
+            filter: '<',
+            providerSelected: "&onProviderSelected",
+        },
+        templateUrl: 'n52.core.provider.provider-selector',
+        controller: ['providerSelectorService',
+            function(providerSelectorService) {
+                this.$onInit = function() {
+                    var list = this.providerList;
+                    if (!Array.isArray(list)) list = Object.keys(list);
+                    this.loadingCount = list.length;
+                    this.providers = [];
+                    list.forEach(url => {
+                        providerSelectorService.fetchProvidersOfAPI(url, this.providerBlacklist, this.filter)
+                            .then(
+                                res => {
+                                    this.loadingCount--;
+                                    res.forEach(entry => {
+                                        if (entry.quantities.platforms > 0) {
+                                            this.providers.push(entry);
+                                        }
+                                    });
+                                },
+                                () => {
+                                    this.loadingCount--;
+                                }
+                            );
+                    });
+                };
+                this.selectProvider = function(provider) {
+                    this.providerSelected({
+                        provider: provider
+                    });
+                };
+            }
+        ]
+    });
