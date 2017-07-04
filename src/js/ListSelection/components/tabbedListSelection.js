900713f40a6a540e16c612125544d814410e28ec
Jan Schulte <j.schulte@52north.org>
2017-07-04 11:58:15 +0200

    change tabbedListSelection

--- src/js/ListSelection/components/tabbedListSelection.js
+++ src/js/ListSelection/components/tabbedListSelection.js
@@ -1,25 +1,33 @@
 angular.module('n52.core.listSelection')
     .component('swcTabbedListSelection', {
         bindings: {
-            onSelectionFinished: '&'
+            onSelectionFinished: '&',
+            providerList: '<'
         },
         templateUrl: 'n52.core.listSelection.tabbed-list-selection',
-        controller: ['timeseriesService', 'serviceFinder', '$location', 'providerService', 'seriesApiInterface',
-            function(timeseriesService, serviceFinder, $location, providerService, seriesApiInterface) {
+        controller: ['timeseriesService', 'serviceFinder', '$location', 'providerService',
+            function(timeseriesService, serviceFinder, $location, providerService) {
 
-                this.providerList = providerService.selectedProviderList;
+                this.$onInit = () => {
+                    if (!(this.providerList instanceof Array)) {
+                        this.providerList = providerService.selectedProviderList;
+                    }
+                };
 
                 this.datasetSelected = function(dataset, url) {
                     // TODO iterate over results
-                    if (dataset[0].datasetType || dataset[0].valueType) {
-                        seriesApiInterface.getDatasets(dataset[0].id, url).then(result => {
-                            serviceFinder.getDatasetPresenter(result.datasetType || result.valueType, result.seriesParameters.platform.platformType, url).presentDataset(result, url);
-                        });
-                    } else {
-                        timeseriesService.addTimeseries(dataset[0]);
-                        $location.url('/diagram');
-                    }
-                    this.onSelectionFinished();
+                    // if (dataset[0].datasetType || dataset[0].valueType) {
+                    //     seriesApiInterface.getDatasets(dataset[0].id, url).then(result => {
+                    //         serviceFinder.getDatasetPresenter(result.datasetType || result.valueType, result.seriesParameters.platform.platformType, url).presentDataset(result, url);
+                    //     });
+                    // } else {
+                    //     timeseriesService.addTimeseries(dataset[0]);
+                    //     $location.url('/diagram');
+                    // }
+                    this.onSelectionFinished({
+                        dataset,
+                        url
+                    });
                 };
             }
         ]
