634d6b628334664593cacbd98f7ae3eb1e9b7d73
Jan Schulte <j.schulte@52north.org>
2017-07-19 11:53:02 +0200

    update tracjetory map selector

--- src/js/selection/trajectory-map-selector/component.js
+++ src/js/selection/trajectory-map-selector/component.js
@@ -7,7 +7,7 @@ angular.module('n52.core.selection')
             serviceUrl: '<',
             filter: '<',
             mapLayers: '<',
-            markerSelected: '&onMarkerSelected'
+            geometrySelected: '&onGeometrySelected'
         },
         templateUrl: 'n52.core.selection.platform-map-selector',
         controller: ['seriesApiInterface', '$scope', 'leafletData',
@@ -25,64 +25,73 @@ angular.module('n52.core.selection')
                     opacity: 1
                 };
 
-                this.markers = {};
                 var layer;
+                var init = false;
+
+                var initMap = (callback) => {
+                    if (!init) {
+                        setTimeout(() => {
+                            leafletData.getMap(this.mapId).then((map) => {
+                                map.invalidateSize();
+                                if (callback) callback();
+                            });
+                        }, 10);
+                    } else {
+                        if (callback) callback();
+                    }
+                };
 
                 this.$onInit = () => {
-                    setTimeout(() => {
-                        leafletData.getMap(this.mapId).then((map) => {
-                            map.invalidateSize();
-                        });
-                    }, 10);
+                    initMap();
                 };
 
                 this.$onChanges = () => {
-                    this.loading = true;
-                    leafletData.getMap(this.mapId).then((map) => {
-                        // clear layer
-                        if (layer) map.removeLayer(layer);
-                        // add marker to layer
-                        seriesApiInterface.getDatasets(null, this.serviceUrl, this.filter)
-                            .then((dataset) => {
-                                if (dataset instanceof Array) {
-                                    var timespan = {
-                                        start: dataset[0].firstValue.timestamp,
-                                        end: dataset[0].lastValue.timestamp
-                                    };
-
-                                    seriesApiInterface.getDatasetData(dataset[0].id, this.serviceUrl, timespan)
-                                        .then(data => {
-                                            if (data.values instanceof Array) {
-                                                layer = L.markerClusterGroup({
-                                                    animate: false
-                                                });
-                                                data.values.forEach(entry => {
-                                                    var geojson = L.geoJson(entry.geometry);
-                                                    geojson.setStyle(defaultStyle);
-                                                    geojson.addTo(map);
-                                                    geojson.on('click', () => {
-                                                        this.markerSelected({
-                                                            dataset: dataset[0],
-                                                            selectedGeometry: entry
-                                                        });
+                    initMap(() => {
+                        this.loading = true;
+                        leafletData.getMap(this.mapId).then((map) => {
+                            // clear layer
+                            if (layer) map.removeLayer(layer);
+                            // add geometries to layer
+                            seriesApiInterface.getDatasets(null, this.serviceUrl, this.filter)
+                                .then((dataset) => {
+                                    if (dataset instanceof Array) {
+                                        var timespan = {
+                                            start: dataset[0].firstValue.timestamp,
+                                            end: dataset[0].lastValue.timestamp
+                                        };
+                                        seriesApiInterface.getDatasetData(dataset[0].id, this.serviceUrl, timespan)
+                                            .then(data => {
+                                                if (data.values instanceof Array) {
+                                                    layer = L.markerClusterGroup({
+                                                        animate: false
                                                     });
-                                                    geojson.on('mouseover', () => {
-                                                        geojson.setStyle(hightlightStyle);
-                                                        geojson.bringToFront();
-                                                    });
-                                                    geojson.on('mouseout', () => {
+                                                    data.values.forEach(entry => {
+                                                        var geojson = L.geoJson(entry.geometry);
                                                         geojson.setStyle(defaultStyle);
+                                                        geojson.addTo(map);
+                                                        geojson.on('click', () => {
+                                                            this.geometrySelected({
+                                                                dataset: dataset[0],
+                                                                selectedGeometry: entry
+                                                            });
+                                                        });
+                                                        geojson.on('mouseover', () => {
+                                                            geojson.setStyle(hightlightStyle);
+                                                            geojson.bringToFront();
+                                                        });
+                                                        geojson.on('mouseout', () => {
+                                                            geojson.setStyle(defaultStyle);
+                                                        });
+                                                        layer.addLayer(geojson);
                                                     });
-                                                    layer.addLayer(geojson);
-                                                });
-                                                layer.addTo(map);
-                                                map.fitBounds(layer.getBounds());
-                                            }
-                                            map.invalidateSize();
-                                            this.loading = false;
-                                        });
-                                }
-                            });
+                                                    layer.addTo(map);
+                                                    map.fitBounds(layer.getBounds());
+                                                }
+                                                this.loading = false;
+                                            });
+                                    }
+                                });
+                        });
                     });
                 };
             }
