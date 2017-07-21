c402cebbd2e29101d0e20daaca526b97064cda06
Jan Schulte <j.schulte@52north.org>
2017-07-21 08:35:35 +0200

    improve mobile components

--- src/js/selection/multi-service-filter-selector/component.js
+++ src/js/selection/multi-service-filter-selector/component.js
@@ -17,10 +17,10 @@ angular.module('n52.core.selection')
                     this.loading--;
                     res.forEach(entry => {
                         var filter = {
-                            filter : prevfilter,
-                            url : url,
-                            itemId : entry.id,
-                            serviceID : serviceID
+                            filter: prevfilter,
+                            url: url,
+                            itemId: entry.id,
+                            serviceID: serviceID
                         };
                         var item = this.items.find((elem) => {
                             if (elem.label === entry.label) return true;
@@ -39,40 +39,55 @@ angular.module('n52.core.selection')
                     this.filterList.forEach(entry => {
                         this.loading++;
                         var filter = entry.filter || {};
+                        var url = entry.url || entry.providerUrl;
                         filter.service = entry.serviceID;
                         switch (this.endpoint) {
                             case 'offering':
-                                seriesApiInterface.getOfferings(null, entry.url, filter)
+                                seriesApiInterface.getOfferings(null, url, filter)
                                     .then(
-                                        res => setItems(res, filter, entry.url, entry.serviceID),
+                                        res => setItems(res, filter, url, entry.serviceID),
                                         () => this.loading--
                                     );
                                 break;
                             case 'phenomenon':
-                                seriesApiInterface.getPhenomena(null, entry.url, filter)
+                                seriesApiInterface.getPhenomena(null, url, filter)
                                     .then(
-                                        res => setItems(res, filter, entry.url, entry.serviceID),
+                                        res => setItems(res, filter, url, entry.serviceID),
                                         () => this.loading--
                                     );
                                 break;
                             case 'procedure':
-                                seriesApiInterface.getProcedures(null, entry.url, filter)
+                                seriesApiInterface.getProcedures(null, url, filter)
                                     .then(
-                                        res => setItems(res, filter, entry.url, entry.serviceID),
+                                        res => setItems(res, filter, url, entry.serviceID),
                                         () => this.loading--
                                     );
                                 break;
                             case 'feature':
-                                seriesApiInterface.getFeatures(null, entry.url, filter)
+                                seriesApiInterface.getFeatures(null, url, filter)
                                     .then(
-                                        res => setItems(res, filter, entry.url, entry.serviceID),
+                                        res => setItems(res, filter, url, entry.serviceID),
                                         () => this.loading--
                                     );
                                 break;
                             case 'category':
-                                seriesApiInterface.getCategories(null, entry.url, filter)
+                                seriesApiInterface.getCategories(null, url, filter)
                                     .then(
-                                        res => setItems(res, filter, entry.url, entry.serviceID),
+                                        res => setItems(res, filter, url, entry.serviceID),
+                                        () => this.loading--
+                                    );
+                                break;
+                            case 'platform':
+                                seriesApiInterface.getPlatforms(null, url, filter)
+                                    .then(
+                                        res => setItems(res, filter, url, entry.serviceID),
+                                        () => this.loading--
+                                    );
+                                break;
+                            case 'dataset':
+                                seriesApiInterface.getDatasets(null, url, filter)
+                                    .then(
+                                        res => setItems(res, filter, url, entry.serviceID),
                                         () => this.loading--
                                     );
                                 break;
